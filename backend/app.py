5469e342896cac4e5e9530909d6ce40afa4ba029
Vignesh <157633158+VigneshSSN26@users.noreply.github.com>
2025-08-12 15:08:14 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -266,4 +266,6 @@ class LSTM(nn.Module):
         return out
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+     # Hugging Face provides the port to run on in an environment variable
+    port = int(os.environ.get('PORT', 7860))
+    app.run(host='0.0.0.0', port=port)
